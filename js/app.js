e3c215af2d6b9fc2c4dca330b2e35bfe3d3abaad
peter <peter.k.nets@gmail.com>
2015-06-15 12:52:25 +0300

    Add a temporary function to show add-task form

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
 /**
  * Created by Peter on 15.06.2015.
  */
+$(document).ready(function(){
+    $('.task_list').addTask();
+});
+
+#.fn.addTask = function() {
+    this.each(function(){
+        $(this).on('click.add_task', '.add_task_button', function(){
+            event.preventDefault();
+            $(this).closest('.task_list').find('.task_add_form').fadeToggle();
+        });
+    });
+};
