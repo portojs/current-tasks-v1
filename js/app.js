44c720a84a61d73f05da66524f04dd07e01d4d41
peter <peter.k.nets@gmail.com>
2015-06-17 17:34:24 +0300

    Add show/hide feature for Add Task Form

--- js/app.js
+++ js/app.js
@@ -5,11 +5,14 @@ $.fn.addTask = function() {
     this.each(function(){
         $(this).on('click.add_task', '.add_task_button', function(){
             event.preventDefault();
-            $(this).closest('.task_list').find('.task_add_form').fadeToggle();
+            $(this).closest('.task_list').find('.add_task_form').toggleClass('hidden');
         });
     });
 };
+$.fn.initialize = function() {
+    $('.add_task_form').addClass('hidden');
+};
 $(document).ready(function(){
     $('.task_list').addTask();
-    $('.task_add_form').addClass('hidden');
+    $('.main_window').initialize();
 });
