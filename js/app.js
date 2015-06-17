9375fe3712bd54ffe4f0433764adef07cad5476b
peter <peter.k.nets@gmail.com>
2015-06-17 17:50:28 +0300

    Add some form elements - to be cont

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 /**
  * Created by Peter on 15.06.2015.
  */
-$.fn.addTask = function() {
+$.fn.openAddTaskForm = function() {
     this.each(function(){
         $(this).on('click.add_task', '.add_task_button', function(){
             event.preventDefault();
@@ -11,8 +11,17 @@ $.fn.addTask = function() {
 };
 $.fn.initialize = function() {
     $('.add_task_form').addClass('hidden');
+    var mustDoTasks = [];
 };
+
+function General() {
+    this.addTask =  function() {
+// not finished
+    };
+    $('.add_task_form').on('click.add_task', 'add-task', this.addTask);
+}
+
 $(document).ready(function(){
-    $('.task_list').addTask();
+    $('.task_list').openAddTaskForm();
     $('.main_window').initialize();
 });
